25c8d9f7b5bb504d8b4b031054ed63d59849c8e1
Arkadijs Makarenko <arkadijs.makarenko@gmail.com>
2025-08-15 10:58:55 +0200

    Initial Commit

---
+++ StartAnAuction/StartAnAuctionApp.swift
@@ -0,0 +1,17 @@
+//
+//  StartAnAuctionApp.swift
+//  StartAnAuction
+//
+//  Created by Arkadijs Makarenko on 15/08/2025.
+//
+
+import SwiftUI
+
+@main
+struct StartAnAuctionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
